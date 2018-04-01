3381ea665b8fec2bffbc37dcd2f42ae2d2f3d0cd
Raven <raven@cs.stanford.edu>
2018-03-31 21:53:23 -0700

    Mobile viewport

--- generate.py
+++ generate.py
@@ -10,6 +10,7 @@ template = '''
 <html>
   <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>For Sharon</title>
     <link rel="stylesheet" href="css/style.css">
   </head>
